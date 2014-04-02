ad5acb62c60d0b1c903b6d7ab74ebad41bf080b3
Jón Orri Kristjánsson <jonorri333@gmail.com>
2014-04-02 10:24:50 +0000

    Removing unused web projects. And testing out webappinterface in the android app

--- WhatToDo/src/is/jok/whattodo/MainActivity.java
+++ WhatToDo/src/is/jok/whattodo/MainActivity.java
@@ -1,22 +1,61 @@
 package is.jok.whattodo;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.content.Context;
+import android.os.Bundle;
 import android.view.Menu;
+import android.webkit.JavascriptInterface;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.widget.Toast;
 
-public class MainActivity extends Activity {
-
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
-	}
-
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
-	}
-
+public class MainActivity extends Activity
+{
+	// TODO: KRAPP http://adndevblog.typepad.com/cloud_and_mobile/2013/08/writing-android-apps-with-html5-javascript.html
+     WebAppInterface _interface;
+    
+     @Override
+     protected void onCreate(Bundle savedInstanceState)
+     {
+           super.onCreate(savedInstanceState);
+           setContentView(R.layout.activity_main);
+          
+           setTitle("KRAPP");
+          
+           WebView webView = (WebView) findViewById(R.id.webview);
+           WebSettings webSettings = webView.getSettings();
+           webSettings.setJavaScriptEnabled(true);
+          
+           _interface = new WebAppInterface(this);
+          
+           webView.addJavascriptInterface(
+                     _interface,
+                     "WebAppInterface");
+          
+           webView.loadUrl("file:///android_asset/Index.html");
+     }
+ 
+     @Override
+     public boolean onCreateOptionsMenu(Menu menu)
+     {
+           getMenuInflater().inflate(R.menu.main, menu);
+           return true;
+     }
+    
+     public class WebAppInterface
+     {
+         Context _context;
+ 
+         /** Instantiate the interface and set the context */
+         WebAppInterface(Context context) {
+         _context = context;
+         }
+ 
+         /** Show a toast from the web page */
+         @JavascriptInterface
+         public void showToast(String msg)
+         {
+             Toast.makeText(_context, msg, Toast.LENGTH_SHORT).show();
+         }
+     }
 }
